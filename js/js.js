2f75c11ac36173853fbd24a75a35ea491db3494d
behruz171 <behruzzo662@gmail.com>
2022-12-10 14:14:46 +0500

    Katalog

---
+++ js/js.js
@@ -0,0 +1,52 @@
+let katalog_massiv = [
+    {
+        photo: "img/Rectangle 24.png",
+        name: "Cake",
+        narx: 10
+    },
+    {
+        photo: "img/Rectangle 28.png",
+        name: "Pizza",
+        narx: 5
+    },
+    {
+        photo: "img/Rectangle 31.png",
+        name: "Coca-cola",
+        narx: 20
+    },
+    {
+        photo: "img/Rectangle 33.png",
+        name: "Salat",
+        narx: 11
+    },
+    {
+        photo: "img/Rectangle 35.png",
+        name: "Meat",
+        narx: 100
+    },
+    {
+        photo: "img/Rectangle 37.png",
+        name: "Meat pro",
+        narx: 200
+    },
+    {
+        photo: "img/Rectangle 38.png",
+        name: "Meat pro",
+        narx: 200
+    },
+    {
+        photo: "img/Rectangle 40.png",
+        name: "Meat pro",
+        narx: 200
+    },
+    {
+        photo: "img/Rectangle 42.png",
+        name: "Meat pro",
+        narx: 200
+      },
+      {
+        photo: "img/Rectangle 46.png",
+        name: "Meat pro",
+        narx: 200
+      }
+]
